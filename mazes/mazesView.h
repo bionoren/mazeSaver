a08a99cbd04be3537593c7afe0bb21ff9c17cdb9
Bion Oren <bion@modopayments.com>
2020-11-02 15:13:56 -0600

    Initial Commit

---
+++ mazes/mazesView.h
@@ -0,0 +1,12 @@
+//
+//  mazesView.h
+//  mazes
+//
+//  Created by Bion Oren on 11/2/20.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface mazesView : ScreenSaverView
+
+@end
---
+++ mazes/mazesView.m
@@ -0,0 +1,51 @@
+//
+//  mazesView.m
+//  mazes
+//
+//  Created by Bion Oren on 11/2/20.
+//
+
+#import "mazesView.h"
+
+@implementation mazesView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
